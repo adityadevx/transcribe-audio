ce9f44aef53cedc2c186f1c8af56c17e8a30206e
Aditya Raj Chauhan <102533268+caringcoder@users.noreply.github.com>
2023-05-10 16:37:18 +0530

    link issues

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -120,7 +120,7 @@ export default function Navbar() {
                     <Box pb={4} display={{ md: 'none' }}>
                         <Stack as={'nav'} spacing={4}>
                             {Links.map((link) => (
-                                <NavLink key={link}>{link}</NavLink>
+                                <NavLink key={link} href={`/${link.toLowerCase()}`}>{link}</NavLink>
                             ))}
                         </Stack>
                     </Box>
